720ebd4edbd8e53caf767b88ffc4e6fd52e5b5fa
bigtree <bigtree11235433@gmail.com>
2025-01-11 23:04:12 +0800

    Initial Commit

---
+++ flushTests/flushTests.swift
@@ -0,0 +1,17 @@
+//
+//  flushTests.swift
+//  flushTests
+//
+//  Created by 孙韬奋 on 2025/1/11.
+//
+
+import Testing
+@testable import flush
+
+struct flushTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
